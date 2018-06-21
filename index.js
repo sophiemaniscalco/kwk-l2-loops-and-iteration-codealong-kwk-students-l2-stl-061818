479286c59b2b3955883437de4e17a779bf4c1c4a
sophiemaniscalco <sophmaniscalco@gmail.com>
2018-06-21 16:41:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+function tailsNeverFails() {
+  let tails = 0;
+  
+  while(Math.random() >= 0.000000001) {
+    tails++
+  }
+  console.log (`You got ${tails} tails in a row!`);
+  
+}
+
+tailsNeverFails();
